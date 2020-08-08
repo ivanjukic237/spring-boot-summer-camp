2a70db378c93afc1ab3a5e6e3981a09428217f1c
ivanjukic <ivanjukic237@gmail.com>
2020-08-08 19:51:33 +0200

    Adding default constructor in Symptom

--- src/main/java/com/ag04smarts/sha/models/patient/medicalRecord/Symptom.java
+++ src/main/java/com/ag04smarts/sha/models/patient/medicalRecord/Symptom.java
@@ -2,6 +2,7 @@ package com.ag04smarts.sha.models.patient.medicalRecord;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -19,7 +20,11 @@ public class Symptom {
 
     @ManyToMany(mappedBy = "symptoms")
     @EqualsAndHashCode.Exclude
+    @ToString.Exclude
     private Set<PatientMedicalRecord> patientMedicalRecords;
+
+    public Symptom() {
+    }
 
     public Symptom(String description) {
         this.description = description;
