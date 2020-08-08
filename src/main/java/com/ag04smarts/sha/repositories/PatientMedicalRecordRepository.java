12a5537a30207e8624202ac5a333853ee36cbf67
ivanjukic <ivanjukic237@gmail.com>
2020-08-08 19:46:24 +0200

    [SHA-8] added query for getting patient id's for patients with given symptoms

--- src/main/java/com/ag04smarts/sha/repositories/PatientMedicalRecordRepository.java
+++ src/main/java/com/ag04smarts/sha/repositories/PatientMedicalRecordRepository.java
@@ -1,9 +1,18 @@
 package com.ag04smarts.sha.repositories;
 
 import com.ag04smarts.sha.models.patient.medicalRecord.PatientMedicalRecord;
+import com.ag04smarts.sha.models.patient.medicalRecord.Symptom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface PatientMedicalRecordRepository extends JpaRepository<PatientMedicalRecord, Long> {
+
+    @Query(value = "select patient.id from PATIENT join PATIENT_MEDICAL_RECORD_SYMPTOM on PATIENT_MEDICAL_RECORD_SYMPTOM.PATIENT_MEDICAL_RECORD_ID = PATIENT.PATIENT_MEDICAL_RECORD_ID where PATIENT_MEDICAL_RECORD_SYMPTOM.SYMPTOM_ID = 1 or PATIENT_MEDICAL_RECORD_SYMPTOM.SYMPTOM_ID = 5", nativeQuery = true)
+    public Collection<Long> getBySymptom();
+    
 }
