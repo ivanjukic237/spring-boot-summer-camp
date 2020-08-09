35848140faad085e47ec467b481bb0771ce664b2
ivanjukic <ivanjukic237@gmail.com>
2020-08-09 21:05:57 +0200

    [SCC-14] Adding MockMvc test to PatientController, adding Junit5 Gradle Dependencies

---
+++ src/test/java/com/ag04smarts/sha/controllers/patient/PatientControllerTest.java
@@ -0,0 +1,34 @@
+package com.ag04smarts.sha.controllers.patient;
+
+import com.ag04smarts.sha.services.patient.PatientService;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class PatientControllerTest {
+
+    @Mock
+    PatientService patientService;
+
+    PatientController patientController;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        patientController = new PatientController(patientService);
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(patientController).build();
+        mockMvc.perform(get("/api/patient"))
+                .andExpect(status().isOk());
+    }
+}
