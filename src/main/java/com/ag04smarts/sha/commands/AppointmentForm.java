6a4cbc3becb6f1fdbd70cc8779b705c0afb78009
ivanjukic <ivanjukic237@gmail.com>
2020-08-19 16:36:57 +0200

    Adding Swagger to AppointmentForm

--- src/main/java/com/ag04smarts/sha/commands/AppointmentForm.java
+++ src/main/java/com/ag04smarts/sha/commands/AppointmentForm.java
@@ -1,5 +1,6 @@
 package com.ag04smarts.sha.commands;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,9 +13,13 @@ import java.util.Date;
 @NoArgsConstructor
 public class AppointmentForm {
 
+    @ApiModelProperty(value = "ID of the patient.", required = true)
     private Long patientId;
+
+    @ApiModelProperty(value = "ID of the doctor.", required = true)
     private Long doctorId;
 
+    @ApiModelProperty(value = "Date of the appointment", required = true)
     @DateTimeFormat(pattern="yyyy-MM-dd")
     private Date appointmentDate;
 
